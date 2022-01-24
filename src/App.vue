330f11a41417f651f9642139b5a41369feaa9082
Stéphane Bressani <stephane@bressani.dev>
2022-01-24 18:02:35 +0100

    serve

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="app" class="bg">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <p></p>
-    <HelloWorld msg="Astrologie"/>
+  <div id="app" class="center">
+    <div class="bg1">
+      <div class="bg2">
+        <img alt="Vue logo" src="./assets/logo.png" class="logo">
+        <p></p>
+        <HelloWorld msg="Astrologie"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +28,31 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
-.bg {
-  background-image: url("~@/assets/bg.png");
+.center {
+  width: 1300px;
+  display: inline-block;
 }
+.bg2 {
+  border-radius: 25px;
+  background-image: linear-gradient(to right, #ffeedd, transparent),
+  linear-gradient(to left, #ffeedd, transparent) !important;
+  background-repeat: repeat-y, repeat-y;
+  background-position: left top, right top;
+  background-size: 300px 100%, 300px 100%;
+  min-height: 100vh;
+}
+.bg1 {
+  display: inline-block;
+  background-image: url("~@/assets/bg.png");
+  background-repeat: no-repeat;
+}
+.logo {
+  border-radius: 25px;
+  background-color: rgba(204, 153, 51, .1);
+  padding: 20px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 </style>
